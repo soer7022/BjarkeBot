d7b00fb11593dfa38a032b89b81d8672d34a3a16
Søren Skov Eriksen <soer7033@gmail.com>
2020-04-16 00:24:14 +0200

    Implement basic command handler

---
+++ bot.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Add commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Bjarke er klar!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    console.log(command);
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('find dig sku da en anden hobby!');
+    }
+});
+
+client.login(token);
